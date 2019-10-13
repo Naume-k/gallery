93daebec2c8ac9652f197a44b2e29cda47e9f198
Naume-k <kobnaome@gmail.com>
2019-10-13 14:22:45 +0200

    added the views of the app

---
+++ pics/views.py
@@ -0,0 +1,41 @@
+# import datetime as dt
+from django.http  import HttpResponse, Http404
+from django.shortcuts import render,redirect
+from .models import Image,Category, Location, Image
+
+# Create your views here.
+
+def image_list(request):
+    images = Image.image_list()
+    return render(request, 'index.html',{"images":images})
+
+
+def search_results(request):
+
+    if 'image_category' in request.GET and request.GET["image_category"]:
+        searched_category = request.GET.get("image_category")
+        images = Image.search_by_category(searched_category)
+        message = f"{searched_category}"
+
+        return render(request, 'search.html',{"message":message,"images":images})
+
+    else:
+        message = "Enter a category"
+        return render(request, 'search.html',{"message":message})
+
+def image(request,image_id):
+    try:
+        image = Image.objects.get(id = image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,{"image":image, id: image_id})
+
+def location_filter(request,image_location_id):
+    images = Image.filter_by_location(id=image_location_id)
+    return render(request,'location.html',{"images":images,"image_location_id":image_location_id})
+
+# def location_filter(request,locate_id):
+
+#     images = Image.objects.filter(image_location=locate_id)
+#     image_location= Location.get_image_location()
+#     return render(request,'location.html',{"images":images,"locations":location})
